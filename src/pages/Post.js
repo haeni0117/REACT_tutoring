8c2eca2a452e4d19e247f3af321032c475db3254
haeni0117 <zion0117@ewhain.net>
2021-07-18 15:34:06 +0900

    🏞 7월 17일 튜터링 스크린샷 업로드

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,24 +1,122 @@
 /** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/react";
-import react from "react";
-import PostImg from '../assets/images/image1.jpeg';
-import MyProfile from '../assets/images/프로필.jpeg';
+import styled from "@emotion/styled";
+import react, { useState } from "react";
+import PostImg from "../assets/images/image1.jpeg";
+import MyProfile from "../assets/images/프로필.jpeg";
 
 const Post = () => {
-    return (
+  const [day, setDay] = useState(1);
+  return (
     //<><div>난 여락이들이야 만나서 반가워 ㅎㅎ</div>
-    <div css={css`display:flex;`}>
-    <img css={css`width:600px;height:600px;`} src={PostImg}/>
-    <div>
-        <div css={css`width:40px;height:40px;border-radius:70%;overflow:hidden;`}> 
-        <img css={css`width :100%;height :100%;`} src = {MyProfile}/>
+    <div
+      css={css`
+        display: flex;
+        background-color: white;
+      `}
+    >
+      <img
+        css={css`
+          width: 600px;
+          height: 600px;
+        `}
+        src={PostImg}
+      />
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          padding: 15px 17px 20px 15px;
+        `}
+      >
+        <div
+          css={css`
+            background-color: yellowgreen;
+            display: flex;
+            padding-bottom: 15px;
+          `}
+        >
+          <div>
+            <StyledProfile src={PostImg} />
+          </div>
+          <div>
+            <div
+              css={css`
+                font-weight: bold;
+                margin-bottom: 3px;
+              `}
+            >
+              yeonjuice
+            </div>
+
+            <div>Chiang Mai,Thailand</div>
+          </div>
         </div>
-        <div>
-        <span>youlakk</span>
-        <span css={css`color:orange;`}>태국 파얍대학교 뒷골목에 있던 카페🌽 정말 행복해보인당💛💛💛</span>
+        <div
+          css={css`
+            background-color: pink;
+            display: flex;
+          `}
+        >
+          <div>
+            <StyledProfile src={PostImg} />
+          </div>
+
+          <div>
+            <Styledmargin
+              css={css`
+                font-weight: bold;
+              `}
+              href="https://www.google.com"
+            >
+              youlakk
+            </Styledmargin>
+            <span
+              css={css`
+                color: black;
+              `}
+            >
+              태국 파얍대학교 뒷골목에 있던 카페🌽 정말 행sdsd보인당💛💛💛
+            </span>
+            <div
+              css={css`
+                margin-top: 25px;
+                margin-bottom: 20px;
+                color: navy;
+              `}
+            >
+              <Styledmargin href="https://www.google.com/search?q=%EA%B3%A0%EA%B3%A0%ED%8C%AC%EC%B8%A0+3%EC%84%B8%EB%8C%80&ei=QprqYNmPFsem0wSYw4mIBQ&oq=%EA%B3%A0%EA%B3%A0%ED%8C%AC%EC%B8%A0+3%EC%84%B8%EB%8C%80&gs_lcp=Cgdnd3Mtd2l6EAM6CAgAELEDEIMBOgUIABCxAzoECAAQCjoCCAA6BAgAEEM6BwgAELEDEEM6BggAEAUQHkoECEEYAFD6CFjQLGDaMWgNcAB4AoABhgGIAaYRkgEEMS4xOZgBAKABAaoBB2d3cy13aXqwAQDAAQE&sclient=gws-wiz&ved=0ahUKEwjZuLyCvNrxAhVH05QKHZhhAlEQ4dUDCA4&uact=5">
+                #고고팬츠
+              </Styledmargin>
+            </div>
+            <StyledDay
+              onClick={() => {
+                setDay(day + 1);
+              }}
+            >
+              {day}일
+            </StyledDay>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-    );
+  );
 };
 export default Post;
+
+const StyledProfile = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 70%;
+  overflow: hidden;
+  margin-right: 16px;
+`;
+
+const StyledDay = styled.div`
+  font-size: 16px;
+  color: rgb(142, 142, 142);
+`;
+const Styledmargin = styled.a`
+  text-decoration: none;
+  color: black;
+`;
